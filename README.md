001cc1982ab72cbabed746cec01c9860b6bc04ba
Elliot Hershberg <eahershberg@gmail.com>
2021-01-13 09:28:38 -0800

    Refactor

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![Integration](https://github.com/elliothershberg/jb2-plugin-setup/workflows/Integration/badge.svg?branch=main)
+jbrowse-plugin-template ![Integration](https://github.com/elliothershberg/jb2-plugin-setup/workflows/Integration/badge.svg?branch=main)
 
 > Template to quickly start a new JBrowse plugin
 
@@ -110,4 +110,8 @@ JavaScript for development. If using only JavaScript, you can change
 `src/index.ts` to `src/index.js`. If using only TypeScript, you can remove
 `"allowJs": true` from `tsconfig.json` and `"@babel/preset-react"` from
 `.babelrc` (and from "devDependencies" in `package.json`).
+
+
+
+
 
--- scripts/setup.js
+++ scripts/setup.js
@@ -6,31 +6,30 @@ Update package.json
 ****************************
 */
 
-let initialPackageJSON = require('../package.json')
-let newPackageJSON = Object.assign({}, initialPackageJSON)
-let projectName = initialPackageJSON.name
+const packageJSON = require('../package.json')
+const { name: projectName } = packageJSON
 
 // ensure setup hasn't already been run
-if (initialPackageJSON['jbrowse-plugin'].name !== 'MyProject') {
+if (packageJSON['jbrowse-plugin'].name !== 'MyProject') {
   console.log(
-    'It appears that setup has already been run. Terminating to avoid overwriting information.',
+    'Warning: It appears that setup has already been run. Terminating to avoid overwriting information.',
   )
   process.exit(1)
 }
 
 // 1. Change "name" in the "jbrowse-plugin" field to the name of your project (e.g. "MyProject")
-newPackageJSON['jbrowse-plugin'].name = projectName
+packageJSON['jbrowse-plugin'].name = projectName
 
 // 2. In the "scripts" field, replace the default name with the name of your project, prefixed with "JBrowsePlugin" in the "start" and "build" entries
-newPackageJSON.scripts.start = `tsdx watch --verbose --noClean --format umd --name JBrowsePlugin${projectName} --onFirstSuccess \"yarn serve --cors --listen 9000 .\""`
+packageJSON.scripts.start = `tsdx watch --verbose --noClean --format umd --name JBrowsePlugin${projectName} --onFirstSuccess \"yarn serve --cors --listen 9000 .\""`
 
-newPackageJSON.scripts.build = `tsdx build --format cjs,esm,umd --name JBrowsePlugin${projectName}`
+packageJSON.scripts.build = `tsdx build --format cjs,esm,umd --name JBrowsePlugin${projectName}`
 
 // 3. In the "module" field, replace jbrowse-plugin-my-project with the name of your project (leave off the @myscope if using a scoped package name) (you can double-check that the filename is correct after running the build step below and comparing the filename to the file in the dist/ folder)
-newPackageJSON.module = `dist/${projectName}.esm.js`
+packageJSON.module = `dist/${projectName}.esm.js`
 
 // this overwrites package.json
-writeJSON(newPackageJSON, 'package.json')
+writeJSON(packageJSON, 'package.json')
 
 /* 
 ****************************
@@ -39,10 +38,9 @@ Update jbrowse_config.json
 */
 
 // replace default plugin name with project name
-let initialJBrowseConfig = require('../jbrowse_config.json')
-let newJBrowseConfig = Object.assign({}, initialJBrowseConfig)
-newJBrowseConfig.plugins[0].name = projectName
-writeJSON(newJBrowseConfig, 'jbrowse_config.json')
+const jbrowseConfig = require('../jbrowse_config.json')
+jbrowseConfig.plugins[0].name = projectName
+writeJSON(jbrowseConfig, 'jbrowse_config.json')
 
 /* 
 ****************************
@@ -52,11 +50,11 @@ Update badge in README
 
 // Replace repo path for integration badge
 let README = readFile('README.md').split(/\r?\n/)
-README[0] = `jbrowse-plugin-template ![Integration](${initialPackageJSON.repository.slice(
+README[0] = `jbrowse-plugin-template ![Integration](${packageJSON.repository.slice(
   0,
   -4,
 )}/workflows/Integration/badge.svg?branch=main)`
-let stream = fs.createWriteStream('README.md')
+const stream = fs.createWriteStream('README.md')
 README.forEach((line) => stream.write(`${line}\r\n`))
 
 /* 
